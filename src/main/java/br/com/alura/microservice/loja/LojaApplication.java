fc420f02e0f2dc8c205c9a83bec0f8ed02b00514
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-07-10 21:58:30 -0300

    adicionando configuracoes eureka

--- src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -1,13 +1,19 @@
 package br.com.alura.microservice.loja;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import br.com.alura.microservice.loja.service.CompraService;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LojaApplication {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
