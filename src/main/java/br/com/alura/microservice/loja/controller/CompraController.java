c21be148f61283392e92a7bd9a54a58ee9ad0d95
Paulo Afonso Batista Gonçalves <paulo.abg@hotmail.com>
2021-07-16 07:55:35 -0300

    implementando bulkhead com Hystrix

--- src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
+++ src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -1,6 +1,8 @@
 package br.com.alura.microservice.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,11 @@ public class CompraController {
 	@PostMapping
 	public Compra realizaCompra(@RequestBody CompraDto compra) {
 		return compraService.realizarCompra(compra);
+	}
+	
+	
+	@GetMapping("/{id}")
+	public Compra getById(@PathVariable Long id) {
+		return compraService.getById(id);
 	}
 }
--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -12,38 +12,52 @@ import br.com.alura.microservice.loja.controller.dto.CompraDto;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDto;
 import br.com.alura.microservice.loja.controller.dto.InfoPedidoDto;
 import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
 public class CompraService {
-	
+
 	private static Logger LOG = LoggerFactory.getLogger(CompraService.class);
-	
+
 	@Autowired
 	private FornecedorClient fornecedorClient;
-	
-	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
+
+	@Autowired
+	private CompraRepository compraRepository;
+
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback",
+			threadPoolKey = "realizarCompraThreadPool")
 	public Compra realizarCompra(CompraDto compra) {
 		final String estado = compra.getEndereco().getEstado();
-		
+
 		LOG.info("Buscando informações do fornecedor de {}", estado);
 		InfoFornecedorDto info = fornecedorClient.getInfoPorEstado(estado);
-		
+
 		LOG.info("Realizando um pedido");
 		InfoPedidoDto infoPedido = fornecedorClient.realizaPedido(compra.getItens());
-		
+
 		Compra compraSalva = new Compra();
 		compraSalva.setPedidoId(infoPedido.getId());
 		compraSalva.setTempoDePreparo(infoPedido.getTempoDePreparo());
 		compraSalva.setEnderecoDestino(info.getEndereco());
-	
+
+		compraRepository.save(compraSalva);
+
 		return compraSalva;
 	}
-	
+
+	@HystrixCommand(threadPoolKey = "getByIdThreadPool")
+	public Compra getById(Long id) {
+		return compraRepository.findById(id).orElseThrow();
+	}
+
 	public Compra realizaCompraFallback(CompraDto compra) {
 		Compra compraFallback = new Compra();
 		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
-	
+
 		return compraFallback;
 	}
+
+
 
 }
